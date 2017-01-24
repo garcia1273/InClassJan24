3fbf92564c7ec01dd92d1c760f2ac5fbd74639c6
rgarcia <rgarcia@B11-R111E-12>
2017-01-24 14:54:30 -0800

    Add project files.

---
+++ InClassJan24/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace InClassJan24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVendorInvoices = new System.Windows.Forms.ListBox();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstVendorInvoices
+            // 
+            this.lstVendorInvoices.FormattingEnabled = true;
+            this.lstVendorInvoices.Location = new System.Drawing.Point(22, 33);
+            this.lstVendorInvoices.Name = "lstVendorInvoices";
+            this.lstVendorInvoices.Size = new System.Drawing.Size(259, 186);
+            this.lstVendorInvoices.TabIndex = 0;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(47, 246);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(200, 23);
+            this.btnLoadData.TabIndex = 1;
+            this.btnLoadData.Text = "Load Vendors and Invoices";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 353);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.lstVendorInvoices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstVendorInvoices;
+        private System.Windows.Forms.Button btnLoadData;
+    }
+}
+
---
+++ InClassJan24/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InClassJan24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome!");
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+
+            LoadVedorAndInvoices();
+        }
+        public static void LoadVedorAndInvoices()
+        {
+            AccountsPayableEntities db = new AccountsPayableEntities();
+            //var vendorList = (from v in db.Vendors
+            //                           join i in db.Invoices on
+            //                           v.VendorID equals i.VendorID
+            //                           select new {v.VendorName, i.InvoiceNumber }).ToList();
+
+            //foreach (var v in vendorList)
+            //{
+            //    lstVendorInvoices.Items.Add($"{v.VendorName} : {v.InvoiceNumber}");
+            //}
+
+
+            //make sure to add System.Data.Entity
+            var vendorList = db.Vendors.Where(v => v.VendorState == "CA").Include(v => v.Invoices)
+                .OrderBy(v => v.VendorName).Take(10).Skip(10).ToList();
+        }
+
+    }
+}
